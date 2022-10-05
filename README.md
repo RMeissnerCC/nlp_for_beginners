7cc5010d3127e4be622324b77a6d615d64a63f15
Robert Meissner <robert.meissner@codecentric.de>
2022-10-05 15:38:12 +0200

    Cleaning up
    Adding preview generation

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# nlp_for_beginners
+# Einleitung
+
+Ein kleines Tool um automatisch aus Volltext einen Titel, Schlagwörter und Cover-Art zu generieren.
+
+## Vorbereitung
+
+Zur Benutzung werden ein Gratis-Account bei [OpenAI](https://beta.openai.com/) und [NLPCloud](https://nlpcloud.com/home/playground/image-generation) benötigt. 
+Die dort generierten API Keys werden entsprechend dem `.env.example` in `.env` hinterlegt.
+
+Die benötigten Python packages werden mit dem Tool `poetry` und dem Befehl `poetry install` installiert.
+
+Für die deutschen Texte benutze ich ein deutsches Modell, dieses muss explizit heruntergeladen werden:
+
+`python -m spacy download de_dep_news_trf`
+
+## Benutzung
+
+In `resources` eine Textdatei (e.g. .md) mit dem Volltext darin ablegen. Die Datei soll sonst nichts enthalten.
+
+Das Skript `src/blogpost_title.py` ausführen.
+
--- src/blogpost_title.py
+++ src/blogpost_title.py
@@ -1,7 +1,17 @@
 import os
 
+import nlpcloud
+import nltk
 import openai
+import requests
+import spacy as spacy
 from dotenv import load_dotenv
+from keybert import KeyBERT
+from nltk.corpus import stopwords
+from requests import HTTPError
+
+nltk.download('stopwords')
+STOPWORDS = set(stopwords.words("german")).union(set(stopwords.words("english")))
 
 load_dotenv()
 
@@ -23,6 +33,54 @@ def create_title(text: str) -> str:
     return response.choices[0]["text"].replace("\n", "")
 
 
+def create_keywords_mit_davinci(text: str) -> list[str]:
+    # Create keywords with DaVinci2 Model on OpenAI
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=f"Gib eine Liste der fünf wichtigsten Schlagwörtern zurück.\n\n{text}",
+        temperature=0.3,
+        max_tokens=64,
+        top_p=1,
+        frequency_penalty=1,
+        presence_penalty=0
+    )
+
+    return response.choices[0]["text"].replace("\n", "")
+
+
+def create_keywords(fulltext: str) -> list[str]:
+    nlp = spacy.load("de_dep_news_trf")
+
+    keyword_model = KeyBERT(model=nlp)
+    keywords = keyword_model.extract_keywords(fulltext, highlight=False, keyphrase_ngram_range=(1, 1),
+                                              stop_words=list(STOPWORDS))
+
+    return [keyword for keyword, weight in keywords]  # Remove the weights and only return keywords
+
+
+def generate_preview(filename: str, keywords: list[str]):
+    api_key = os.getenv("NLPCLOUD_API_KEY")
+    client = nlpcloud.Client("stable-diffusion", api_key, gpu=True, lang="en")
+    try:
+        response = client.image_generation(" ".join(keywords))
+    except HTTPError:
+        print("Currently no rate available. Skipping")
+        return
+
+    print(f"response {response}")
+
+    response = requests.get(response["url"])
+
+    file_name = f"{filename}_preview.png"
+    # Save the image
+    if response.status_code == 200:
+        with open(file_name, "wb") as f:
+            f.write(response.content)
+        print(f"Preview created for '{filename}'.")
+
+
 def determine_title_from_text(filename: str):
     # read file and make text out of it
     with open(f"../resources/{filename}") as file:
@@ -35,6 +93,15 @@ def determine_title_from_text(filename: str):
     # create title
     title_from_davinci: str = create_title(fulltext)
     print(f"Title for '{filename}': {title_from_davinci}")
+
+    keywords = create_keywords(fulltext)
+    print(f"KeyBERT Keywords for '{filename}': {' '.join(keywords)}")
+
+    keywords = create_keywords_mit_davinci(fulltext)
+    print(keywords, type(keywords))
+    print(f"Davinci2 Keywords for '{filename}': {keywords}")
+
+    generate_preview(filename, keywords)
 
 
 if __name__ == '__main__':
